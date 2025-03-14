5236af06bb5506e5ddfe9c0a7200a1ac361f2ed2
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-14 22:36:24 +0700

    14/3/2025

--- B7_Nhan dien bang video,cam.py
+++ B7_Nhan dien bang video,cam.py
@@ -1,1 +1,15 @@
 
+import cv2
+import numpy as np
+import face_recognition as fr
+import os #load thư viện ảnh
+
+path = "Picture/pic2"
+imges = []
+classNames = []
+#load tên toàn bộ ảnh trong thư mục vào list
+myList = os.listdir(path)
+
+
+
+print(myList)
